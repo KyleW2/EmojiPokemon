47e1156bc258300a1db54e9723ffcab60cd00330
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-10-23 17:47:01 -0500

    Added constants file

---
+++ code/game_constants.py
@@ -0,0 +1,7 @@
+TRAINER_EMOJIS = "😀😁😂😎😍🤩😴🤐🙄😝🤑😤😨🥶🥵😡🤢🤡🤠"
+TRAINER_EMOJIS = [emoji for emoji in TRAINER_EMOJIS]
+
+POKEMON_EMOJIS = "🐱🐍🐌🐵🐶🐺🦁🐯🦒🦊🦝🐮🐷🐗🐭🐹🐰🐻🐨🐼🐸🦓🐴🦄🐔🐲🐉🦕🐀🦔🐇🐿🦖🦎🐊🐢🐍🐬🐠🐟"
+POKEMON_EMOJIS = [emoji for emoji in POKEMON_EMOJIS]
+
+PORT = 50051
