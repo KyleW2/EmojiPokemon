c6931594777f40044a46c75f42d40e6ae1df525e
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-10-23 17:28:43 -0500

    Testing supported emojis

---
+++ code/server.py
@@ -0,0 +1,8 @@
+TRAINER_EMOJIS = "👮‍♂️💂‍♂️👷‍♂️👨‍⚕️👨‍🎓👨‍🌾👨‍🍳👨‍🔧👨‍🏭👨‍💼👨‍🔬👨‍🎤👨‍🎨👨‍✈️👨‍🚀👨‍🚒👨‍🏫👨‍⚖️🙍‍♂️"
+TRAINER_EMOJIS = [emoji for emoji in TRAINER_EMOJIS]
+
+POKEMON_EMOJIS = "🐱🐍🐌🐵🐶🐺🦁🐯🦒🦊🦝🐮🐷🐗🐭🐹🐰🐻🐨🐼🐸🦓🐴🦄🐔🐲🐉🦕🐀🦔🐇🐿🦖🦎🐊🐢🐍🐬🐠🐟"
+POKEMON_EMOJIS = [emoji for emoji in POKEMON_EMOJIS]
+
+print(TRAINER_EMOJIS)
+print(POKEMON_EMOJIS)
