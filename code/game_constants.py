444d934985bf4fb88d4562c334cf81a9d0a50b29
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-11-03 19:09:32 -0500

    Added direction strings

--- code/game_constants.py
+++ code/game_constants.py
@@ -3,5 +3,7 @@ TRAINER_EMOJIS = [emoji for emoji in TRAINER_EMOJIS]
 POKEMON_EMOJIS = '🐱🐍🐌🐵🐶🐺🦁🐯🦒🦊🦝🐮🐷🐗🐭🐹🐰🐻🐨🐼🐸🦓🐴🦄🐔🐲🐉🦕🐀🦔🐇🐿🦖🦎🐊🐢🐍🐬🐠🐟'
 POKEMON_EMOJIS = [emoji for emoji in POKEMON_EMOJIS]
 
+DIRECTIONS = ["north", "north_east", "noth_west", "east", "west", "south", "south_east", "south_west"]
+
 PORT = 50051
 GRID_SIZE = 8
