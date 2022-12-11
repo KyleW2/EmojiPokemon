acd793157eb9c8fde10aa22b039e64b5d3bc1a40
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-12-11 17:53:34 -0600

    Added dictionary of direction -> opposite direction

--- code/game_constants.py
+++ code/game_constants.py
@@ -5,5 +5,16 @@ POKEMON_EMOJIS = [emoji for emoji in POKEMON_EMOJIS]
 
 DIRECTIONS = ["north", "north_east", "north_west", "east", "west", "south", "south_east", "south_west"]
 
+OPPOSITE_DIRECTION = {
+    "north"      : "south",
+    "east"       : "west",
+    "south"      : "north",
+    "west"       : "east",
+    "north_east" : "south_west",
+    "south_west" : "north_east",
+    "north_west" : "south_east",
+    "south_east" : "north_west"
+}
+
 PORT = 50051
 GRID_SIZE = 8
