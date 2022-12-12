0d25f0f4d8273df697eecbd9bdef5f4edfcf7ff7
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-12-11 18:06:14 -0600

    Back to 1 trainer and 2 pokemon for easier debugging

--- code/game_constants.py
+++ code/game_constants.py
@@ -2,19 +2,7 @@ TRAINER_EMOJIS = '😀😁😂😎😍🤩😴🤐🙄😝🤑😤😨🥶🥵�
 TRAINER_EMOJIS = [emoji for emoji in TRAINER_EMOJIS]
 POKEMON_EMOJIS = '🐱🐍🐌🐵🐶🐺🦁🐯🦒🦊🦝🐮🐷🐗🐭🐹🐰🐻🐨🐼🐸🦓🐴🦄🐔🐲🐉🦕🐀🦔🐇🐿🦖🦎🐊🐢🐍🐬🐠🐟'
 POKEMON_EMOJIS = [emoji for emoji in POKEMON_EMOJIS]
-
-DIRECTIONS = ["north", "north_east", "north_west", "east", "west", "south", "south_east", "south_west"]
-
-OPPOSITE_DIRECTION = {
-    "north"      : "south",
-    "east"       : "west",
-    "south"      : "north",
-    "west"       : "east",
-    "north_east" : "south_west",
-    "south_west" : "north_east",
-    "north_west" : "south_east",
-    "south_east" : "north_west"
-}
+DIRECTIONS = ['north', 'north_east', 'north_west', 'east', 'west', 'south', 'south_east', 'south_west']
 
 PORT = 50051
-GRID_SIZE = 8
+GRID_SIZE = 10
